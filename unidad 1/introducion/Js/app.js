ad6b414bfedfbcc492cdded56db3e8fcaa2ca266
MichaelGald <michaelcom292@gmail.com>
2024-01-30 20:31:31 -0600

    first commit

---
+++ unidad 1/introducion/Js/app.js
@@ -0,0 +1,10 @@
+//alert("Hola guapo");
+
+const nombre = prompt('¿Cual es su nombre?');
+document.querySelector('.contenido').innerHTML = `${nombre} esta aprendiendo JS.` ;
+
+// una linea
+/*
+varias
+lineas
+*/
